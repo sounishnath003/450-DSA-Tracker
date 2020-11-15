714f9337849603ff7bea5d5eba9efd6faa0f287a
Sounish Nath <flock.sinasini@gmail.com>
2020-11-16 01:25:42 +0530

    project initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mx-auto mt-10 p-8 max-w-4xl m-auto ">
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h2 className="text-4xl text-center">DSA 450 Cracker</h2>
+      <div className="text-indigo-700 text-center text-xl tracking-wide uppercase my-4">your gateway to crack product based</div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
