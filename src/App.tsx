7263e0c20a2aa66a53480afebc21d508a992bcaa
Sounish Nath <flock.sinasini@gmail.com>
2020-11-17 15:20:40 +0530

    Question[] context setted up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import { QuestionData } from "./Backend/db-store/data";
 import Home from "./components/Home/Home";
+import { QuestionDataContext } from "./context/QuestionDataContext";
 
 function App() {
   return (
-    <div className="App bg-white mx-auto mt-10 p-8 max-w-4xl m-auto ">
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </div>
+    <QuestionDataContext.Provider value={QuestionData}>
+      <div className="App bg-white mx-auto mt-10 p-8 max-w-4xl m-auto ">
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </QuestionDataContext.Provider>
   );
 }
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
-import { getData } from "../../Backend/services/database";
+import React, { useContext, useEffect } from "react";
+import { IQuestionData } from "../../Backend/model/Question-model";
+import { QuestionDataContext } from "../../context/QuestionDataContext";
+import QTopicCard from "../QTopicCard";
 
 const Home: React.FC = () => {
-  useEffect(() => {
-    getData();
-  }, []);
+  const questionDataContext: IQuestionData[] = useContext(QuestionDataContext);
+
+  useEffect(() => {}, [questionDataContext]);
 
   return (
     <React.Fragment>
       <h2 className="text-4xl text-center">DSA 450 Cracker</h2>
       <div className="text-indigo-700 text-center text-xl tracking-wide uppercase my-4">
         your gateway to crack product based
+      </div>
+      <div className="mt-8">
+        {questionDataContext.length < 1 ? (
+          <div>loading....</div>
+        ) : (
+          <div className="flex d-flex flex-row flex-wrap justify-evenly m-2">
+            {" "}
+            <QTopicCard {...questionDataContext[0]} />
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
